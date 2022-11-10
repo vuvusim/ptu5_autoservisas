85bab9af9549aaf9c227a84afc8ec1f8f8bcbf8c
simas venskus <simasvenskus@gmail.com>
2022-11-10 10:12:01 +0200

    Views,css,hmtl,static, templates,

--- ptu5_autoservisas/autoservice/views.py
+++ ptu5_autoservisas/autoservice/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import CarModel, Car, Service, Order, OrderLine
 
-# Create your views here.
+def index(request):
+    # return HttpResponse('Sveiki Atvyke!')
+    carmodel_count = CarModel.objects.count()
+    car_count = Car.objects.count()
+    service_count = Service.objects.count()
+    order_count = Order.objects.count()
+    orderline_count = OrderLine.objects.count()
+
+    context = {
+        'carmodel_count': carmodel_count, 
+        'car_count': car_count, 
+        'service_count': service_count, 
+        'order_count': order_count, 
+        'orderline_count': orderline_count
+    }
+
+    return render(request, 'autoservice/index.html', context)
+
